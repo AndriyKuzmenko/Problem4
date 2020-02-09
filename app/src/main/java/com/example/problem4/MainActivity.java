89ab08fcdf18b77bbf85e3b357c9bd61eda1d5c7
Andriy <ak8022@bs.amalnet.k12.il>
2020-02-09 20:56:47 +0200

    Code

--- app/src/main/java/com/example/problem4/MainActivity.java
+++ app/src/main/java/com/example/problem4/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.problem4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
+    Button btn;
+    int i;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button);
+        i=0;
+    }
+
+    public void ButtonPressed(View view)
+    {
+        i++;
+        if (i%7!=0)
+        {
+            btn.setText("This is the click number: " + i);
+        }
+        else
+        {
+            btn.setText("BOOM");
+        }
     }
 }
